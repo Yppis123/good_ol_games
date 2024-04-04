27906abadc29f7269d1c69eaf02b78f9ba468097
Yppis123 <115878871+Yppis123@users.noreply.github.com>
2024-04-04 12:37:06 +0200

    Add files via upload

--- Ranking Good ol Games/src/Game.java
+++ Ranking Good ol Games/src/Game.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Game {
+import java.util.ArrayList;
+
+public class Game {
+    private String naam;
+    //    private float originelePrijs; // nog niet zeker of we dit nodig hebben
+    private float prijs;
+    private String genre;
+    private boolean inUitverkoop;
+    private String gemGameScore;
+    private ArrayList<Review> reviewLijst;
+
+
+    public Game() {
+        reviewLijst = new ArrayList<>();
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public float getPrijs() {
+        return prijs;
+    }
+
+    public String getGemGameScore() {
+        return gemGameScore;
+    }
+
+    public void wijzigPrijs(float korting) {
+        prijs *= korting; // korting als kommagetal
+
+        // als prijs is verlaagd, zet in uitverkoop
+        inUitverkoop = korting < 1;
+    }
+
+    public void voegReviewToe(Review review) {
+        reviewLijst.add(review);
+    }
 }
+
--- Ranking Good ol Games/src/Review.java
+++ Ranking Good ol Games/src/Review.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Review {
+public class Review {
+
+    private float scoreGameplay;
+    private float scoreGraphics;
+    private float scoreStoryline;
+    private float gemiddeldeScore;
+    private String toelichting;
+
+    public Review(){
+
+    }
+
+    public void berekenGemiddeldeScore(float scoreGameplay, float scoreGraphics, float scoreStoryline){
+        float totaleScore = scoreGameplay + scoreGraphics + scoreStoryline;
+        int totaleAantalCriteria = 3;
+        gemiddeldeScore = totaleScore/totaleAantalCriteria;
+    }
+
+
 }
