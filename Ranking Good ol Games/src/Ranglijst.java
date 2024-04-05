d30d7b0e213ff28401bb7e7751b969a81e57af17
Yppis123 <115878871+Yppis123@users.noreply.github.com>
2024-04-05 09:22:36 +0200

    Update

--- Ranking Good ol Games/src/Ranglijst.java
+++ Ranking Good ol Games/src/Ranglijst.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Ranglijst {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ranglijst {
+    private String genre;
+
+    public ArrayList<Game> Ranglijst = new ArrayList<Game> ();
+
+
+    public void toonRanglijst(){
+        //System.out.println(Ranglijst);
+    }
+
 }
--- Ranking Good ol Games/src/Uitverkoop.java
+++ Ranking Good ol Games/src/Uitverkoop.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Uitverkoop {
+import java.util.ArrayList;
+
+
+public class Uitverkoop {
+    ArrayList<Game> uitverkooplijst = new ArrayList<Game>();
+
+    public void toonUitverkoop() {
+        for (int i = 0; i < uitverkooplijst.size(); i++) {
+            System.out.println("Naam: " + uitverkooplijst.get(i).getNaam());
+            System.out.println("Prijs: " + uitverkooplijst.get(i).getPrijs());
+            System.out.println("Gemiddelde game score: " + uitverkooplijst.get(i).getGemGameScore());
+            System.out.println();
+        }
+    }
 }
